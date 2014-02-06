43a7372e0c00a642f384ab0679be1a37ea4a5f78
Alex Reynolds <cam00674@myport.ac.uk>
2014-02-06 10:33:22 +0000

    Remove obselete template

--- public_html/templates/searchPanels/resultSmall/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/php/preload.php');
-$recipeSearch = $request->response;
-$results = new searchResults($recipeSearch);
-
-$results->outputHTML();
-$results->returnJSON();
-?>
