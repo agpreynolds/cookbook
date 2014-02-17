d590505472dfffe95521f77d183f40a3540aa436
Alex Reynolds <cam00674@myport.ac.uk>
2014-02-17 19:08:47 +0000

    Search works :)

--- public_html/php/classes/controller/recipeSearch.php
+++ public_html/php/classes/controller/recipeSearch.php
@@ -19,9 +19,15 @@ class recipeSearch extends validateForm {
 		$where = "?uri a recipe:Recipe ; 
 				rdfs:label ?label ;				
 				rdf:author ?author ";
-		$where .= $this->addCuisineTriples($this->formData['recipe:Cuisine']);
-		$where .= $this->addCourseTriples($this->formData['recipe:Course']);
-		$where .= $this->addIngredientsTriples($this->formData['recipe:Food']);
+		
+		$where .= $this->addCuisineTriples($this->formData['recipe:Cuisine']
+			,$this->formData['recipe:Cuisine_type']);
+
+		$where .= $this->addCourseTriples($this->formData['recipe:Course']
+			,$this->formData['recipe:Course_type']);
+
+		$where .= $this->addIngredientsTriples($this->formData['recipe:Food']
+			,$this->formData['recipe:Food_type']);
 		$where .= ".\n ?author rdfs:label ?username";
 
 		$query = array(
@@ -36,37 +42,52 @@ class recipeSearch extends validateForm {
 		
 		return $results = $arcDb->query2($query);
 	}
-	private function addIngredientsTriples($ingredients) {
+	private function addIngredientsTriples($ingredients,$type) {
 		if (isset($ingredients)) {
 			$qIngredients = ";\n recipe:ingredients ?ingredients .
 				?ingredients";
 			$qFood = '';
 		
 			for ( $i=1; $i<=count($ingredients); $i++ ) {
-				$qIngredients .= " ?p{$i} ?s{$i}";
-				$qIngredients .= ( $i != count($ingredients) ) ? ';' : '';
-				$qFood .= ".\n ?s{$i} a recipe:Ingredient ;\n
-					recipe:food <{$ingredients[$i-1]}>";
+				if ( $type == 'all' ) {
+					$qIngredients .= " ?p{$i} ?s{$i}" . ( $i != count($ingredients) ? ';' : '' );
+					$qFood .= ".\n ?s{$i} a recipe:Ingredient ;\n
+						recipe:food <{$ingredients[$i-1]}>";					
+				}
+				elseif ( $type == 'one') {
+					$qIngredients .= ( $i == 1 ? " ?p ?s" : '' );
+					$qFood .= ( $i==1 ? ".\n ?s a recipe:Ingredient ;\n" : '') . "{ ?s recipe:food <{$ingredients[$i-1]}> }" . ( $i != count($cuisine)-1 ? 'UNION' : '' );
+				}
 			}
 
 			return $qIngredients . $qFood;
 		}
 	}
-	private function addCuisineTriples($cuisine) {
+	private function addCuisineTriples($cuisine,$type) {
 		if (isset($cuisine)) {
 			$OUT = '';
-			foreach ($cuisine as $c) {
-				$OUT .= ";\n recipe:cuisine <{$c}>";
-			}
+			for ($i=0; $i<count($cuisine); $i++) {
+				if ( $type == 'all' ) {
+					$OUT .= ";\n recipe:cuisine <{$cuisine[$i]}>";
+				}
+				elseif ( $type == 'one' ) {
+					$OUT .= ( $i==0 ? ".\n" : '') . "{ ?uri recipe:cuisine <{$cuisine[$i]}> }" . ( $i < count($cuisine)-1 ? 'UNION' : '' );
+				}
+			}				
 			return $OUT;			
 		}
 	}
-	private function addCourseTriples($course) {
+	private function addCourseTriples($course,$type) {
 		if (isset($course)) {
 			$OUT = '';
-			foreach ($course as $c) {
-				$OUT .= ";\n recipe:course <{$c}>";
-			}
+			for ($i=0; $i<count($course); $i++) {
+				if ( $type == 'all' ) {
+					$OUT .= ";\n recipe:course <{$course[$i]}>";
+				}
+				elseif ( $type == 'one' ) {
+					$OUT .= ( $i==0 ? ".\n" : '') . "{ ?uri recipe:course <{$course[$i]}> }" . ( $i < count($course)-1 ? 'UNION' : '' );
+				}
+			}				
 			return $OUT;			
 		}
 	}
