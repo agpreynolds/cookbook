7fa742c8ac75501df1b6b5ad21fa8d36aa700fa8
Alex Reynolds <cam00674@myport.ac.uk>
2014-02-27 16:53:03 +0000

    Fix error messages on signup

--- public_html/php/forms/userSignup.php
+++ public_html/php/forms/userSignup.php
@@ -36,6 +36,18 @@ return array(
 	'validators' => array(
 		'fieldsIdentical' => array('password','password2')
 	),
+	'errors' => array(
+		'required_username' => "Please enter a username.",
+		'required_password' => "Please enter a password.",
+		'required_password2' => "Please re-enter your password.",
+		'required_terms' => "Please accept the terms of use.",
+		'minLength_password' => "Your password must contain between 6 and 15 characters.",
+		'minLength_password2' => "Your password must contain between 6 and 15 characters.",
+		'maxLength_username' => "Please enter a username with less than 15 characters.",
+		'maxLength_password' => "Your password must contain between 6 and 15 characters.",
+		'maxLength_password2' => "Your password must contain between 6 and 15 characters.",
+		'fieldsIdentical_password' => "The password fields must match."
+	),
 	'onSuccess' => "global.user.signin"
 );
 
